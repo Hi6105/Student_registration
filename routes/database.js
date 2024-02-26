2982501593d4dd97d8147dde4e8270617de1da7c
Hi6105 <ritik6104@gmail.com>
2024-02-27 00:19:36 +0530

    minor change

--- routes/database.js
+++ routes/database.js
@@ -2,8 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const mongoose = require("mongoose");
 
-// Replace <connection-string> with your MongoDB Atlas connection URI
-const uri = `mongodb+srv://hvishwakarma821:${process.env.PASSWORD}@cluster0.4uegh9y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = process.env.URI;
 
 const db = () => {
   mongoose
